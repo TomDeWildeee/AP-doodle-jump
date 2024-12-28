27fa27bbd267c53ae0c9a8c322fe51bdf8ef6397
TomDeWildeee <tomdewilde0@gmail.com>
2024-12-28 04:40:36 +0100

    feat: Added Stopwatch class

---
+++ src/headers/logic/Stopwatch.h
@@ -0,0 +1,25 @@
+#ifndef DOODLE_JUMP_STOPWATCH_H
+#define DOODLE_JUMP_STOPWATCH_H
+
+#include <chrono>
+namespace Logic {
+
+class Stopwatch {
+public:
+    static Stopwatch& getInstance();
+
+    [[nodiscard]] float getDeltaTime() const;
+    void start();
+    void tick();
+
+private:
+    Stopwatch();
+    float deltaTime = 0.0f;
+
+    // Found high_resolution_clock online, not sure if it's the best choice
+    std::chrono::high_resolution_clock::time_point lastTime;
+};
+
+} // namespace Logic
+
+#endif // DOODLE_JUMP_STOPWATCH_H
