794ff54b71e3d7510db925bbf13b729e7b7146c1
TomDeWildeee <tomdewilde0@gmail.com>
2024-12-28 04:36:36 +0100

    feat: Added Random singleton class

---
+++ src/headers/logic/Random.h
@@ -0,0 +1,21 @@
+#ifndef DOODLE_JUMP_RANDOM_H
+#define DOODLE_JUMP_RANDOM_H
+
+#include <random>
+namespace Logic {
+
+class Random {
+public:
+    static Random& getInstance();
+    float getFloat(float min, float max);
+    int getInt(int min, int max);
+
+private:
+    Random();
+    // Found mt19937 online, not sure if it's the best choice
+    std::mt19937 generator;
+};
+
+} // namespace Logic
+
+#endif // DOODLE_JUMP_RANDOM_H
