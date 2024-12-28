7e6c5081c69a9fb668e3e764802ebee72fd7d220
TomDeWildeee <tomdewilde0@gmail.com>
2024-12-28 04:54:45 +0100

    feat: Added Score

---
+++ src/headers/logic/Score.h
@@ -0,0 +1,32 @@
+#ifndef DOODLE_JUMP_SCORE_H
+#define DOODLE_JUMP_SCORE_H
+
+#include "Bonus.h"
+#include "Observer.h"
+#include "Platform.h"
+namespace Logic {
+
+class Score : public Observer {
+public:
+    static Score& getInstance();
+
+    void update() override;
+    void onNewHeight(float height);
+
+    // Added, because I saw they were getting used in scoring as well. Not sure if they are needed
+    void onBonusCollected(BonusType bonusType);
+    void onPlatformReuse(PlatformType platformType);
+
+    [[nodiscard]] int getScore() const;
+    [[nodiscard]] int getHighScore() const;
+
+private:
+    Score();
+    int score = 0;
+    int highScore = 0;
+    float maxHeight = 0.0f;
+};
+
+} // namespace Logic
+
+#endif // DOODLE_JUMP_SCORE_H
---
+++ src/logic/Score.cpp
@@ -0,0 +1,50 @@
+#include "../headers/logic/Score.h"
+
+namespace Logic {
+
+Score& Score::getInstance() {
+    static Score instance;
+    return instance;
+}
+
+void Score::update(){};
+
+void Score::onNewHeight(float height) {
+    if (height > maxHeight) {
+        int newScore = static_cast<int>((height - maxHeight) * 10);
+        score += newScore;
+        maxHeight = height;
+        highScore = std::max(score, highScore);
+    }
+}
+
+void Score::onBonusCollected(BonusType bonusType) {
+    switch (bonusType) {
+    case BonusType::JETPACK:
+        score += 100;
+        break;
+    case BonusType::SPRING:
+        score += 50;
+        break;
+    }
+    highScore = std::max(score, highScore);
+}
+
+void Score::onPlatformReuse(PlatformType platformType) {
+    switch (platformType) {
+    case PlatformType::NORMAL:
+        score -= 10;
+        break;
+    case PlatformType::MOVABLE:
+        score -= 5;
+        break;
+    case PlatformType::DISAPPEARING:
+        score += 15;
+        break;
+    case PlatformType::BREAKABLE:
+        break;
+    }
+    score = std::max(score, highScore);
+}
+
+} // namespace Logic
