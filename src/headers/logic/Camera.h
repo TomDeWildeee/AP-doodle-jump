445d9d4cb7e15effecb491de70e3c744f3a8aefa
TomDeWildeee <tomdewilde0@gmail.com>
2024-12-28 04:47:36 +0100

    feat: Added Camera

---
+++ src/headers/logic/Camera.h
@@ -0,0 +1,24 @@
+#ifndef DOODLE_JUMP_CAMERA_H
+#define DOODLE_JUMP_CAMERA_H
+
+namespace Logic {
+
+class Camera {
+public:
+    Camera(float width, float height);
+    void update(float newY);
+    void worldToScreen(float& x, float& y) const;
+    bool isVisible(float y) const;
+    float getY() const;
+
+private:
+    float width;
+    float height;
+    float y;
+    float minY;
+    float maxY;
+};
+
+} // namespace Logic
+
+#endif // DOODLE_JUMP_CAMERA_H
